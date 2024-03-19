9f2df2fb76d82044df3fcad322c561bc042f9694
EleonoraFlaborea <flaboreaeleonora@gmail.com>
2024-03-19 17:33:15 +0100

    add routes

--- routes/web.php
+++ routes/web.php
@@ -19,15 +19,11 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', GuestHomeController::class)->name('guest.home');
 
-Route::get('/admin',AdminHomeController::class)->middleware(['auth', 'verified'])->name('admin.home');
+Route::prefix('/admin')->name('admin.')->middleware('auth')->group(function(){
+    Route::get('',AdminHomeController::class)->name('admin.home');
+    Route::resource('projects', ProjectController::class);
+});
 
-Route::get('/projects', ProjectController::class, 'index')->name('projects.index');
-Route::get('/projects/create', ProjectController::class, 'create')->name('projects.create');
-Route::get('/projects/{project}', ProjectController::class, 'show')->name('projects.show');
-Route::post('/projects', ProjectController::class, 'store')->name('projects.store');
-Route::get('/projects/{project}/edit', ProjectController::class, 'edit')->name('projects.edit');
-Route::put('/projects/{project}', ProjectController::class, 'update')->name('projects.update');
-Route::delete('/projects/{project}', ProjectController::class, 'destroy')->name('projects.destroy');
 
 
 
