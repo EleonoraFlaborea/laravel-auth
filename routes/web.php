bde769877f9cb0e09a4cbd35460c45556ab3b3f4
EleonoraFlaborea <flaboreaeleonora@gmail.com>
2024-03-19 16:51:41 +0100

    add homecontroller guest

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('guest.home');
-})->name('');
+Route::get('/', HomeController::class)->name('guest.home');
 
 Route::get('/admin', function () {
     return view('admin.home');
