901866bbbd5ce2c2bd41580a604fa8b2a6d8dad7
EleonoraFlaborea <flaboreaeleonora@gmail.com>
2024-03-19 18:01:59 +0100

    add table

--- database/migrations/2024_03_19_161430_create_projects_table.php
+++ database/migrations/2024_03_19_161430_create_projects_table.php
@@ -13,7 +13,13 @@ return new class extends Migration
     {
         Schema::create('projects', function (Blueprint $table) {
             $table->id();
+            $table->string('title', 50)->unique();
+            $table->string('slug')->unique();
+            $table->string('content');
+            $table->string('image')->nullable();
             $table->timestamps();
+            $table->softDeletes();
+
         });
     }
 
